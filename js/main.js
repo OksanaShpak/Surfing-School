0248575fc8986027fc44c9af01dd35a6fa282d45
Oksana <oksana.shpack@gmail.com>
2020-11-07 20:39:37 +1100

    adaptive

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 $(function () {
-    var mixer = mixitup('.gallery__content');
-
     $('.slider-blog__inner').slick({
         dots: true,
         arrows: false
     });
+    
+    $('.menu__btn').on('click',function(){
+        $('.menu__list').toggleClass('menu__list--active');
+    });
+
+
+    var mixer = mixitup('.gallery__content');
 });
